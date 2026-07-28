79009bb1567638500822b1233a6272efaab5b893
root <root@ip-172-31-17-96.ec2.internal>
2026-07-28 13:26:26 +0000

    Mycommit

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public static void main(String[] args) {
+        int rollnumber =143;
+       String name ="Varma";
+       String Job ="AWS Devops Engineer";
+    System.out.println("welcome to Devops Engineering");
+    System.out.println("My Name is "+ name);
+    System.out.println("My Rollnumber is "+ rollnumber);
+    System.out.println("My Job is "+ Job);
+    System.out.println("Thanks You All");
+    
+    }
+}
